6756a91998ed5c1622e91fc70e5d52bcda9db078
wimllie <jbpablico27@gmail.com>
2022-11-28 02:52:26 -0600

    Starting over Javascript

--- script.js
+++ script.js
@@ -6,36 +6,6 @@ const allClearButton = document.querySelectorAll('[data-all-clear]');
 const prevOperandTextElement = document.querySelector('data-prev-operand');
 const curOperandTextElement = document.querySelector('data-cur-operand');
 
-const calculator = new Calculator(prevOperandTextElement, curOperandTextElement);
-
-class Calculator {
-    constructor(prevOperandTextElement, curOperandTextElement) {
-        this.prevOperandTextElement = prevOperandTextElement;
-        this.curOperandTextElement = curOperandTextElement;
-        this.clear();
-    }
-    clear() {
-        this.currentOperand = '';
-        this.prevOperant = '';
-        this.operation = undefined;
-    }
-    delete() {
-
-    }
-    appendNumber(number) {
-        if(number === '.' && this.curOperandTextElement.includes('.')
-        this.currentOperand = this.currentOperand.toString() + number.toString();
-    }
-    chooseOperation(operation) {
-
-    }
-    compute() {
-
-    }
-    updateDisplay() {
-        this.curOperandTextElement.innerText = this.currentOperand;
-    }
-}
 function addition(a,b) {
     var c = a + b;
     return c;
